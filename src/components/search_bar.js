0c071df7f3ce5772c3b0b8f1b18889541d4470eb
kdshin2010 <kdshin2010@gmail.com>
2016-05-18 14:53:36 -0700

    first commit adding react app to remote repository

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+//input hander onChange
+//state is a plain js object record events
+//control field 
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {term: 'Kyle'};
+	}
+	render() {
+		return (
+			<div>
+				<input 
+					value = {this.state.term}
+					onChange={event => this.setState({term: event.target.value})} />
+			}
+			</div>
+		)
+	}
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+// createa new component that should produce html
+//import reactDOM interacts with DOM
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBhYvzkE_emUXUT6fUgJ-qrZP228Lx4DLo'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+//const values cannot change
+//set up constructor
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+		//when you have object where key and value are identical
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			//identical
+		});
+
+	}
+
+	render() {
+		return(
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+
+		);
+	}
+}
+
+
+
+// //Take this component and put in DOM
+
+//createa an instance of passing app with JSX
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
